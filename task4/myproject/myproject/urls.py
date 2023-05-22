9d6096c575bac5a2fadbe3d2f5057884dd3f0e0d
Jinwoo Lee <binarywooo@gmail.com>
2023-05-22 22:35:49 +0900

    feat: implement post

--- task4/myproject/myproject/urls.py
+++ task4/myproject/myproject/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    path('', views.home, name='home'),
+
+    path('post/create/', views.post_create, name='post_create'),
+    path('post/', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/update/', views.post_update, name='post_update'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
